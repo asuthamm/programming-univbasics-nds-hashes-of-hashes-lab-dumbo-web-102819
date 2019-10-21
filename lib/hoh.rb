48e471ae00724f068900f571dba9871bdf46a217
Alex Suthammanont <asuthamm@gmail.com>
2019-10-21 17:37:47 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -45,7 +45,7 @@ def naming_system
   hash = {
     { label: "Kingdom", sub_category: {label: "Phylum", sub_category: {label: "Class"} } },
     { label: "Order"}
-    { label: "Family", sub_category: {label: "Genus",}}
+    { label: "Family", sub_category: {label: "Genus"}}
     
   }
   
